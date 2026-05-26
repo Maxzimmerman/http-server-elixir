9a9802aa440019ad681000705cf22836bd847c84
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-27 00:53:16 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -24,7 +24,7 @@ defmodule Server do
 
     response =
       if request.line["target"] == "/" do
-        IO.puts(request.line["target"])
+        IO.inspect(request.line["target"], label: "TEST")
 
         """
         HTTP/1.1 200 OK\r\n\r\n
