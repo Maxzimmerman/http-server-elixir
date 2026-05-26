d606e3a8806dd12293ad4519c3005eeda3b9c681
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-26 23:58:53 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -15,7 +15,7 @@ defmodule Server do
     # # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
     {:ok, client} = :gen_tcp.accept(socket)
-    :gen_tcp.send(client, "")
+    :gen_tcp.send(client, "HTTP")
   end
 
   def main(_args) do
