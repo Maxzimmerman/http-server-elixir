3a00c1bda2c5f82983342a7c418eb10c278b4b29
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-27 00:54:17 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -43,6 +43,7 @@ defmodule Server do
   def decode_http_request(request) do
     [line, headers | rest] = String.split(request, "\r\n")
     [method, target, version] = String.split(line)
+    IO.inspect(line, label: "TEST 2")
 
     if is_nil(rest) do
       %HTTPRequest{
