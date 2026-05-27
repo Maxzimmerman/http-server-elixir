6528fff16b1b76d7ad77177bbb2fbdc0727dd474
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-27 22:41:54 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -31,7 +31,7 @@ defmodule Server do
           """
           HTTP/1.1 200 OK\r\n
           Content-Type: text/plain\r\n
-          Content-Length: #{String.length(str)}\r\n\r\n
+          Content-Length: #{String.length(str)}\r\n
 
           #{str}
           """
