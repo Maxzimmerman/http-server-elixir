0cdb7fdc6ab910e52ee55365e534e5340ad1e43a
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-27 22:51:37 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -28,16 +28,16 @@ defmodule Server do
           """
 
         "/echo/" <> str ->
-          """
+          "
           HTTP/1.1 200 OK
           \r\n
 
-          Content-Type: text/plain
+          Content-Type: text/plain\r\n
           Content-Length: #{String.length(str)}\r\n
           \r\n
 
           #{str}
-          """
+          "
 
         _ ->
           """
