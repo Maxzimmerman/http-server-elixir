a642d84a3c70a123370c94736996799a9c6fcfda
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-29 00:03:42 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -47,7 +47,7 @@ defmodule Server do
         # File POST endpoint
         %HTTPRequest{
           line: %{target: "/files" <> path},
-          headers: [_, _, "Content-Type: application/octet-stream"]
+          headers: [_, _, "Content-Type: application/octet-stream" | _]
         } ->
           IO.puts("RIGHT")
 
