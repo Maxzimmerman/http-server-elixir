3ac0472b59c7faaad5063f5a486a9b4f5e91c270
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-30 23:08:30 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -48,7 +48,7 @@ defmodule Server do
          headers: [_host, "Accept-Encoding: " <> encoding | _rest]
        }) do
     IO.inspect(encoding, label: "TEST")
-    matches = Enum.filter(String.split(encoding), &Enum.member?(@allowed_encoding_types, &1))
+    matches = Enum.filter(String.split(encoding, ","), &Enum.member?(@allowed_encoding_types, &1))
     IO.inspect(matches, label: "TEST matches")
 
     if encoding in @allowed_encoding_types do
