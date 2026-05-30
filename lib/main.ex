d9012a8889148e69bcd98662d71e84ade5f00042
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-30 22:22:02 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -49,6 +49,8 @@ defmodule Server do
 
     if encoding in @allowed_encoding_types do
       "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: #{encoding}\r\nContent-Length: #{String.length(str)}\r\n\r\n#{str}"
+    else
+      "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{String.length(str)}\r\n\r\n#{str}"
     end
   end
 
